acad84adbff31602b19f57997c85889ddf556662
yidafu <me@yidefu.dev>
2024-01-23 23:20:57 +0800

    chore: update build script

--- setup-ubuntu.sh
+++ setup-ubuntu.sh
@@ -9,3 +9,11 @@ cargo install cargo-xwin
 
 cd swc-jni
 
+cargo zigbuild --release --target x86_64-unknown-linux-gnu
+cargo zigbuild --release --target aarch64-unknown-linux-gnu
+
+mkdir -p ../swc-binding/src/main/resources/linux-x64-gnu
+mkdir -p ../swc-binding/src/main/resources/linux-aarch64-gnu
+
+cp target/arm-unknown-linux-aarch64/release/libswc_jni.so ../swc-binding/src/main/resources/linux-aarch64-gnu
+cp target/x86_64-unknown-linux-gnu/release/libswc_jni.so ../swc-binding/src/main/resources/linux-x64-gnu
