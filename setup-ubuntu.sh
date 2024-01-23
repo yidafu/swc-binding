653429585a38f28074c74901cd4814d1f1917018
yidafu <me@yidefu.dev>
2024-01-23 21:38:36 +0800

    chore: add setup-ubuntu.sh

---
+++ setup-ubuntu.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+rustup target list
+sudo apt update 
+sudo apt install build-essential
+sudo apt install gcc g++
+sudo apt install g++-aarch64-linux-gnu
+sudo apt install gcc-aarch64-linux-gnu
+sudo apt -y install gcc-11-x86-64-linux-gnux32-base
