ac81d93c791a722f2e879079ec880d610270ddc9
yidafu <tiaozuwushi@qq.com>
2025-10-24 23:04:58 +0800

    Clean up swc-generator test outputs
    Remove generated test output files from swc-generator module to clean up the repository and avoid committing generated code.

--- swc-generator/test-outputs/11-function-types/dsl/dev/yidafu/swc/dsl/create.kt
+++
@@ -1,8 +0,0 @@
-package dev.yidafu.swc.dsl
-
-import dev.yidafu.swc.types.FunctionSignatures
-import dev.yidafu.swc.types.FunctionSignaturesImpl
-import kotlin.Unit
-
-public fun createFunctionSignatures(block: FunctionSignatures.() -> Unit): FunctionSignatures =
-    FunctionSignaturesImpl().apply(block)
